68237bdb81b693af54dbb26c28ac88af0e64bb90
Leroy Kats <leroy.kats@student.nhlstenden.com>
2022-05-19 13:43:37 +0200

    Map "tulpen" quest
    Begin gemaakt voor de tulpen quest map

--- DutchAdventures/Assets/Scripts/PlayerMovementLeroy.cs
+++ DutchAdventures/Assets/Scripts/PlayerMovementLeroy.cs
@@ -7,10 +7,12 @@ public class PlayerMovementLeroy : MonoBehaviour
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
 
     }
@@ -21,9 +23,21 @@ public class PlayerMovementLeroy : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove()
+    {
         if (change != Vector3.zero)
         {
             MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
         }
     }
 
