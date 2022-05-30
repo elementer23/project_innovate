10825f945ea90df62b0c3337e58dfe6bef57e833
jeskaikema <jesk.aikema@gmail.com>
2022-05-30 16:31:29 +0100

    quest update
    dialoog werkt en boeken kunnen gelezen worden

--- DutchAdventures/Assets/Scripts/InfoBoard/InfoBoard.cs
+++ DutchAdventures/Assets/Scripts/InfoBoard/InfoBoard.cs
@@ -8,6 +8,11 @@ public class InfoBoard : MonoBehaviour
     
     public CanvasGroup canvasGroup;
     public string text;
+    private int minDist = 2;
+    [SerializeField]
+    private GameObject pointer;
+    public Transform player;
+    public bool canTouch = false;
     public TextMeshProUGUI signText;
     // Start is called before the first frame update
     void Start()
@@ -18,23 +23,29 @@ public class InfoBoard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float dist = Vector2.Distance(player.position, transform.position);
+        canTouch = dist < minDist;
+        pointer.SetActive(canTouch);
     }
 
     private void OnMouseDown()
     {
-        if (canvasGroup.alpha == 0)
+        if (canTouch)
         {
-            canvasGroup.alpha = 1;
-            canvasGroup.interactable = true;
-            canvasGroup.blocksRaycasts = true;
-            signText.text = text;
-        }
-        else
-        {
-            canvasGroup.alpha = 0;
-            canvasGroup.interactable = false;
-            canvasGroup.blocksRaycasts = false;
+
+            if (canvasGroup.alpha == 0)
+            {
+                canvasGroup.alpha = 1;
+                canvasGroup.interactable = true;
+                canvasGroup.blocksRaycasts = true;
+                signText.text = text;
+            }
+            else
+            {
+                canvasGroup.alpha = 0;
+                canvasGroup.interactable = false;
+                canvasGroup.blocksRaycasts = false;
+            }
         }
     }
 
