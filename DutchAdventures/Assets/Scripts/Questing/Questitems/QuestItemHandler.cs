0aa872ac4d4470ed165e5c9327f66bfebc91bf32
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2022-06-07 12:34:59 +0200

    Questsysteem verbeterd

--- DutchAdventures/Assets/Scripts/Questing/Questitems/QuestItemHandler.cs
+++ DutchAdventures/Assets/Scripts/Questing/Questitems/QuestItemHandler.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class QuestItemHandler : MonoBehaviour
 {
-    public Transform player;
-    protected float minDist = 2;
-    private bool canObtain = false;
-    [SerializeField]
-    private GameObject pointer;
     public string keyItem;
+    public string requiredQuest;
+
+    private Transform player;
+    private GameObject pointer;
+    private float minDist = 2;
+    private PlayerQuestHandler playerQuestHandler;
+    private bool canObtain = false;
+    private bool isVisible = false;
 
     private void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerQuestHandler = player.GetComponent<PlayerQuestHandler>();
+        pointer = transform.GetChild(0).gameObject;
         GetComponent<BoxCollider2D>().enabled = false;
         GetComponent<BoxCollider2D>().enabled = true;
     }
@@ -20,8 +26,11 @@ public class QuestItemHandler : MonoBehaviour
     void Update()
     {
         float dist = Vector2.Distance(player.position, transform.position);
-        canObtain = dist < minDist;
+        canObtain = dist < minDist && isVisible;
         pointer.SetActive(canObtain);
+
+        isVisible = playerQuestHandler.getQuest().title == requiredQuest;
+        GetComponent<SpriteRenderer>().enabled = isVisible;
     }
 
     //Pickups quest item if the player press down and is in range
@@ -29,23 +38,14 @@ public class QuestItemHandler : MonoBehaviour
     {
         if (canObtain)
         {
-            //StartCoroutine(Testcor());
-            KeyItemsSaver keyItemSaver = player.GetComponent<KeyItemsSaver>();
-            keyItemSaver.setItem(keyItem, true);
-            keyItemSaver.SaveItems();
+            if (isVisible)
+            {
+                KeyItemsSaver keyItemSaver = player.GetComponent<KeyItemsSaver>();
+                keyItemSaver.setItem(keyItem, true);
+                keyItemSaver.SaveItems();
 
-            gameObject.SetActive(false);
-            //player.GetComponent<KeyItemsHandler>().setItem(keyItem, true);
-            //player.GetComponent<JsonWriter>().WriteJson();
-            //gameObject.SetActive(false);
+                Destroy(gameObject);
+            }
         }
     }
-
-    //IEnumerator Testcor()
-    //{
-    //    player.GetComponent<KeyItemsHandler>().setItem(keyItem, true);
-    //    yield return new WaitForSeconds(0.1f);
-    //    player.GetComponent<keyItemsSaver>().WriteJson();
-    //    gameObject.SetActive(false);
-    //}
 }
