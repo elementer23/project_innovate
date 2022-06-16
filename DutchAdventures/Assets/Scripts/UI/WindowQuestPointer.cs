c6496aaed808522ebc78eb0436483f3e0ea510a7
robin <robenjay@hotmail.nl>
2022-06-16 17:28:19 +0200

    pointer disappearance when close to target

--- DutchAdventures/Assets/Scripts/UI/WindowQuestPointer.cs
+++ DutchAdventures/Assets/Scripts/UI/WindowQuestPointer.cs
@@ -14,10 +14,14 @@ public class WindowQuestPointer : MonoBehaviour
     private GameObject closestNpc;
     [SerializeField]
     private bool QuestNPC = false;
+
+    private Camera cam;
+
     private void Start()
     {
         npcs = GameObject.FindGameObjectsWithTag("NPC");
         closestNpc = npcs[0];
+        cam = UnityEngine.Camera.main;
 
     }
 
@@ -25,10 +29,12 @@ public class WindowQuestPointer : MonoBehaviour
     {
         foreach (GameObject npc in npcs)
         {
-            if (!npc.GetComponent<NPCController>().quest.title.Equals(string.Empty))
+            if (npc.GetComponent<NPCController>().isQuestGiver)
             {
-                if (player.GetComponent<KeyItemsSaver>().hasItem(npc.GetComponent<NPCController>().getRequiredKeyitem()))
-                {
+                //!npc.GetComponent<NPCController>().quest.title.Equals(string.Empty)
+                //if (!player.GetComponent<KeyItemsSaver>().hasItem(npc.GetComponent<NPCController>().getRequiredKeyitem()))
+                //{
+                    Debug.Log(!npc.GetComponent<NPCController>().quest.requestedItem.Equals(string.Empty));
                     QuestNPC = true;
                     float playerPos = Vector2.Distance(npc.transform.position, player.position);
                     float closestNpcPos = Vector2.Distance(closestNpc.transform.position, player.position);
@@ -37,7 +43,17 @@ public class WindowQuestPointer : MonoBehaviour
                     {
                         closestNpc = npc;
                     }
+
+                Vector3 viewPos = cam.ViewportToWorldPoint(closestNpc.transform.position);
+                if (viewPos.x >= 0 && viewPos.x <= 1 && viewPos.y >= 0 && viewPos.y <= 1 && viewPos.z > 0)
+                {
+                    QuestNPC = false;
                 }
+                else
+                {
+                    QuestNPC = true;
+                }
+                //}
             }
         }
         gameObject.SetActive(QuestNPC);
