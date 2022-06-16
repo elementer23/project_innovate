b77627b50c62dd12d125005046a4183413216d36
SteffanvanderWerf <Steffan.van.der.werf@student.nhlstenden.com>
2022-06-16 14:01:15 +0200

    fixed continue button after finising exame

--- DutchAdventures/Assets/Scripts/Quizmanager.cs
+++ DutchAdventures/Assets/Scripts/Quizmanager.cs
@@ -7,42 +7,86 @@ using TMPro;
 
 public class Quizmanager : MonoBehaviour
 {
+    [Header("Quiz")]
+    public GameObject Quizpanel;
     public List<QuestionAndAnswers> QnA;
+    public List<QuestionAndAnswers> BackupQnA;
     public GameObject[] options;
     public int currentQuestion;
-
-    public GameObject Quizpanel;
-    //public GameObject GoPanel;
-
     public TextMeshProUGUI QuestionTxt;
-    //public Text ScoreTxt;
-
-    int totalQuestions = 0;
+   
+    [Header("Result panel")]
+    public int totalQuestions = 0;
     public int score;
+    public GameObject EndScore;
+    public TextMeshProUGUI PassedFailedTxt;
+    public TextMeshProUGUI ScoreTxt;
+
+    [Header("Minigame")]
+    [SerializeField]
+    private GameObject world;
+    [SerializeField]
+    private GameObject mingame;
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private PlayerSpawn exitPos;
+    [SerializeField]
+    private GameObject retryBtn;
+    [SerializeField]
+    private GameObject continueBtn;
 
     private void Start()
     {
         totalQuestions = QnA.Count;
-        //GoPanel.SetActive(false);
+        EndScore.SetActive(false);
         generateQuestion();
+        
     }
 
     public void retry()
     {
+        Debug.Log(player.position);
+        exitPos.spawnPosition = player.position;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void continueBtnClick() {
+
+        KeyItemsSaver keyItemSaver = player.GetComponent<KeyItemsSaver>();
+        keyItemSaver.setItem("TestPassed", true);
+        
+        world.SetActive(true);
+        mingame.SetActive(false);
+
+        Debug.Log("Minigame compleeted");
+
     }
 
     void GameOver()
     {
         Quizpanel.SetActive(false);
-        //GoPanel.SetActive(true);
-        //ScoreTxt.text = score + "/" + totalQuestions;
+        EndScore.SetActive(true);
+
+        if (totalQuestions == score)
+        {
+            PassedFailedTxt.text = "You have PASSED the test.";
+            continueBtn.SetActive(true);
+            retryBtn.SetActive(false);
+        } else 
+        {
+            PassedFailedTxt.text = "Your have FAILED the test.";
+            continueBtn.SetActive(false);
+            retryBtn.SetActive(true);
+        }
+        ScoreTxt.text = "You scored " + score + "/" + totalQuestions + " points.";
     }
 
     public void correct()
     {
         //when you are right
         score += 1;
+        Debug.Log("CurrentQuest"+currentQuestion);
         QnA.RemoveAt(currentQuestion);
         StartCoroutine(waitForNext());
     }
@@ -50,6 +94,8 @@ public class Quizmanager : MonoBehaviour
     public void wrong()
     {
         //when you answer wrong
+        Debug.Log("CurrentQuest" + currentQuestion);
+        BackupQnA.AddRange(QnA);
         QnA.RemoveAt(currentQuestion);
         StartCoroutine(waitForNext());
     }
