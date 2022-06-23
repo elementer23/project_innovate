46b3e6ef03fd4b5bed9f24efadab5e10f41960a3
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2022-06-23 14:59:42 +0200

    NPC's gefixed in southscene

--- DutchAdventures/Assets/Scripts/UI/WindowQuestPointer.cs
+++ DutchAdventures/Assets/Scripts/UI/WindowQuestPointer.cs
@@ -22,17 +22,21 @@ public class WindowQuestPointer : MonoBehaviour
         //find all npc with the NPC tag
         npcs = GameObject.FindGameObjectsWithTag("NPC");
         questNpcs = new List<GameObject>();
-        
+
         //fill the questnpcs list
         FillQuestNPCList();
 
         //get the main camera
         cam = Camera.main;
         getCanvas = GetComponent<CanvasGroup>();
+
+        //get the player
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     private void Update()
     {
+        npcs = GameObject.FindGameObjectsWithTag("NPC");
         FillQuestNPCList();
         if (questNpcs.Count > 0 && player.GetComponent<PlayerQuestHandler>().getQuest().title.Equals(string.Empty))
         {
@@ -49,40 +53,60 @@ public class WindowQuestPointer : MonoBehaviour
         //loop through all the npcs with a quest in current scene
         foreach (GameObject npc in questNpcs)
         {
-            //check whether an npc has a active quest or not
-            int Icon = npc.GetComponent<NPCController>().transform.childCount;
-            bool QuestIconStatus = npc.GetComponent<NPCController>().transform.GetChild(Icon - 1).GetComponent<QuestIcon>().activeQuest;
-
-            if (QuestIconStatus == true)
+            bool hasQuest = false;
+            if (!npc.GetComponent<NPCController>().requiredItem.Equals(string.Empty))
             {
-                //get the player position and position of closest npc with a quest
-                float playerPos = Vector2.Distance(npc.transform.position, player.position);
-                float closestNpcPos = Vector2.Distance(npc.transform.position, player.position);
-
-                UpdateArrow(npc);
-                DisappearArrow(npc);
+                hasQuest = npc.GetComponent<NPCController>().hasRequiredItem;
             }
-            
+            else if (npc.GetComponent<NPCController>().requiredItem.Equals(string.Empty))
+            {
+                hasQuest = true;
+            }
+
+            if (hasQuest)
+            {
+                if (npc.transform.Find("QuestMarker(Clone)"))
+                {
+                    //check whether an npc has a active quest or not
+                    bool QuestIconStatus = npc.transform.Find("QuestMarker(Clone)").GetComponent<QuestIcon>().activeQuest;
+
+                    if (QuestIconStatus == true)
+                    {
+                        //get the player position and position of closest npc with a quest
+                        float playerPos = Vector2.Distance(npc.transform.position, player.position);
+                        float closestNpcPos = Vector2.Distance(npc.transform.position, player.position);
+
+                        UpdateArrow(npc);
+                        DisappearArrow(npc);
+                    }
+                }
+            }
         }
-        
+        if (questNpcs.Count == 0)
+        {
+            getCanvas.alpha = 0;
+        }
     }
 
     private void FillQuestNPCList()
     {
         //clear the npc quest list 
         questNpcs.Clear();
-        
+
         //loop through all the npcs in the current scene
         foreach (var npc in npcs)
         {
             //check whether an npc has a quest title or not
             if (!npc.GetComponent<NPCController>().quest.title.Equals(string.Empty))
             {
-                //add the npcs with a quest to a list
-                questNpcs.Add(npc);
+                if (!npc.GetComponent<NPCController>().hasCompletedQuest)
+                {
+                    //add the npcs with a quest to a list
+                    questNpcs.Add(npc);
+                }
             }
         }
-        
+
         if (questNpcs.Count > 0)
         {
             //set the first npc that can be found as the closest npc
